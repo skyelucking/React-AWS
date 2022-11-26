8b26cf36a08117e73f819dcb83873a16d5c1d294
skyelucking <lucking.skye@gmail.com>
2022-11-25 21:55:10 -0700

    using example app

---
+++ build/precache-manifest.114486d9beee16f9a848312953ce375f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7489dabf44dcde613f4eb61db5776707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e089209d42871db4d0bd",
+    "url": "/static/css/main.e2bfd105.chunk.css"
+  },
+  {
+    "revision": "a63fa0c807e0b9e95faa",
+    "url": "/static/js/2.f0ff0f6b.chunk.js"
+  },
+  {
+    "revision": "e089209d42871db4d0bd",
+    "url": "/static/js/main.279e1a0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
